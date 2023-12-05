54c18575a0162a4bc98064da4e9dfee7112970c6
elvina-s <rammnuschka@gmail.com>
2023-12-05 16:17:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [anIdea, setAnIdea] = useState("");
+
+  useEffect(() => {
+    fetchAnIdea();
+  },[])
+
+  const fetchAnIdea = async () => {
+    const response = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    setAnIdea(data.activity)
+  }
+
+  return ( <div>
+    <div className="container">
+      <h1>Bored and don't know what to do?</h1>
+      <h2>Click the button below to get an idea!</h2>
+      <button onClick={fetchAnIdea}>Get an idea</button>
+      <h3>{anIdea}</h3>
     </div>
+  </div>
   );
 }
 
