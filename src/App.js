1a129be846dcb563f586205908498bab9e3329ee
elvina-s <rammnuschka@gmail.com>
2023-12-08 15:59:58 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   },[])
 
   const fetchAnIdea = async () => {
-    const response = await fetch("http://www.boredapi.com/api/activity/");
+    const response = await fetch("https://www.boredapi.com/api/activity/");
     const data = await response.json();
     setAnIdea(data.activity)
   }
